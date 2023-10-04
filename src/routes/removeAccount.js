edfeaaf4016c3044cdf22f9ddcd9305fd2df2d8a
sohkey <thomas.rodriguez38530@gmail.com>
2023-10-04 16:38:36 +0200

    fix: use id instead of email to delete account

--- src/routes/removeAccount.js
+++ src/routes/removeAccount.js
@@ -5,7 +5,7 @@ import bcrypt from 'bcrypt'
 export default (app) => {
     app.post('/api/removeuser', auth, (req, res) => {
         console.log(req.body);
-        User.findOne({ where: { email: req.body.email } }).then(user => {
+        User.findOne({ where: { id: req.body.id } }).then(user => {
             if(!user) {
                 const message = `L'utilisateur demandé n'existe pas.`
                 return res.status(404).json({ message })
@@ -21,11 +21,11 @@ export default (app) => {
             
             User.destroy({
                 where: {
-                    email: req.body.email
+                    id: req.body.id
                 }
-            }).then(email => {
-                    const message = `L'utilisateur ${req.body.email} has been deleted`
-                    res.json({ message, email })
+            }).then(() => {
+                    const message = `Account has been deleted`
+                    res.json({ message })
                 })
                 .catch(error => {
                     const message = 'Error while deleting user'
