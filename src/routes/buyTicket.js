bf15cc59554c8ef0bd527bd80d87751395166c81
sohkey <thomas.rodriguez38530@gmail.com>
2023-10-06 16:47:23 +0200

    buy ticket

--- src/routes/buyTicket.js
+++ src/routes/buyTicket.js
@@ -1,32 +1,59 @@
 import { Ticket } from '../db/sequelize.js'
-import { ValidationError, UniqueConstraintError} from 'sequelize'
+import { ValidationError, UniqueConstraintError } from 'sequelize'
 
 export default (app) => {
     app.post('/api/ticket/buy', (req, res) => {
         console.log(req.body)
         const {
             transaction,
-            festival
-        } = res.body
-        
-        if(transaction) {
+            UserId,
+            FestivalId,
+            amount
+        } = req.body
+
+        if (transaction) {
+            if (amount > 1) {
+                for (let index = 0; index < amount; index++) {
+                    Ticket.create({
+                        transaction,
+                        UserId,
+                        FestivalId
+                    }).then((ticket) => {
+                        const message = `Le ticket pour ${transaction} a bien été créer.`
+                        res.json({ message, ticket })
+                        number--
+                    })
+                        .catch(error => {
+                            if (error instanceof ValidationError) {
+                                return res.status(400).json({ message: error.message, error })
+                            }
+                            if (error instanceof UniqueConstraintError) {
+                                return res.status(400).json({ message: error.message, error })
+                            }
+                            const message = 'Le ticket n\'a pas pu être créer. Réessayer dans quelques intants'
+                            res.status(500).json({ message, error })
+                        })
+
+                }
+            }
             Ticket.create({
                 transaction,
-                festival
+                UserId,
+                FestivalId
             }).then((ticket) => {
-                const message = `Le ticket pour ${req.body.festival} a bien été créer.`
-                res.json({ message, festival, transaction })
-              })
-              .catch(error => {
-                if (error instanceof ValidationError) {
-                  return res.status(400).json({ message: error.message, error})
-                }
-                if (error instanceof UniqueConstraintError) {
-                  return res.status(400).json({ message: error.message, error})
-                }
-                const message = 'Le ticket n\'a pas pu être créer. Réessayer dans quelques intants'
-                res.status(500).json({ message, data: error })
-              })
+                const message = `Le ticket pour ${transaction} a bien été créer.`
+                res.json({ message, ticket })
+            })
+                .catch(error => {
+                    if (error instanceof ValidationError) {
+                        return res.status(400).json({ message: error.message, error })
+                    }
+                    if (error instanceof UniqueConstraintError) {
+                        return res.status(400).json({ message: error.message, error })
+                    }
+                    const message = 'Le ticket n\'a pas pu être créer. Réessayer dans quelques intants'
+                    res.status(500).json({ message, error })
+                })
         }
     })
 }
